2793b60295de61863468938fbb7379ff5581fb16
andre <andref9@al.insper.edu.br>
2022-11-29 20:25:10 -0300

    debug

--- sprites.py
+++ sprites.py
@@ -19,10 +19,6 @@ class Ship(pygame.sprite.Sprite):
         self.groups = groups
         self.assets = assets
 
-        # Só será possível atirar uma vez a cada 500 milissegundos
-        self.last_shot = pygame.time.get_ticks()
-        self.shoot_ticks = 500
-
     def update(self):
         # Atualização da posição da nave
         
@@ -36,23 +32,6 @@ class Ship(pygame.sprite.Sprite):
             self.rect.bottom = HEIGHT
         if self.rect.top < 0:
             self.rect.top = 0
-
-
-    def shoot(self):
-        # Verifica se pode atirar
-        now = pygame.time.get_ticks()
-        # Verifica quantos ticks se passaram desde o último tiro.
-        elapsed_ticks = now - self.last_shot
-
-        # Se já pode atirar novamente...
-        if elapsed_ticks > self.shoot_ticks:
-            # Marca o tick da nova imagem.
-            self.last_shot = now
-            # A nova bala vai ser criada logo acima e no centro horizontal da nave
-            new_bullet = Bullet(self.assets, self.rect.top, self.rect.centerx)
-            self.groups['all_sprites'].add(new_bullet)
-            self.groups['all_bullets'].add(new_bullet)
-            self.assets[PEW_SOUND].play()
 
 class Predio(pygame.sprite.Sprite):
     def __init__(self, assets):
